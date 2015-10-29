072924c1189010f6c9bb1c4a6dce544c173d3ee9
Jos <gnu.crazier@gmail.com>
2015-10-29 13:49:22 +0800

    DFA usage

--- src/DFAGen/DFAGen.cc
+++ src/DFAGen/DFAGen.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include "DFA.hh"
 
 using namespace std;
 #define STATE_NUM 15
@@ -9,65 +10,63 @@ using namespace std;
 using state_t = unsigned short;
 using case_t = unsigned char;
 
-case_t index_of(int c) {
-	if(c > 0 && c < 128) return c;
-	return 0;
-}
-
 void dump_content(std::ostream& is)
 {
-	map<state_t, map<case_t, state_t>> lmap;
+    DFA<state_t, case_t> dfa(STATE_NUM, CASE_NUM);
+    
+    #define L(From, Case, To)           \
+    do {                                \
+        dfa.link(From, Case, To);       \
+    } while(0)
 
-	for(size_t i = 0; i < STATE_NUM; ++i) {
-		for(size_t j = 0; j < CASE_NUM; ++j)
-			lmap[i][j] = 0;
-	}
-
-	#define T(From, Case, To)		\
-	do {					\
-		lmap[From][index_of(Case)] = To;	\
-	} while(0)
+    #define UL(From, Case)              \
+    do {                                \
+        dfa.unlink(From, Case);         \
+    } while(0)
 
 	std::string alpha_symbol("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_+-*/\\=<>!&");
 	std::string alpha_number_symbol("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_+-*/\\=<>!&");
 	std::string number("0123456789");
 
-	for(char c : alpha_symbol) {
-		T(1, c, 2);
-		T(3, c, 2);
-		T(4, c, 2);
-		T(5, c, 2);
-	}
-	for(char c : alpha_number_symbol)
-		T(2, c, 2);
-	for(char c : number) {
-		T(1, c, 4);
-		T(3, c, 4);
-		T(4, c, 4);
-		T(5, c, 5);
-	}
-	for(unsigned char c = 0; c < CASE_NUM; ++c) {
-		T(10, c, 10);
-		T(12, c, 12);
-		T(14, c, 12);
-	}
-	T(1, '"', 12);
-	T(1, '-', 3);
-	T(1, ';', 10);
-	T(1, ' ', 11);
-	T(1, '\t', 11);
-	T(1, '\n', 11);
-	T(1, '{', 6);
-	T(1, '}', 7);
-	T(1, '(', 8);
-	T(1, ')', 9);
-	T(4, '.', 5);
-	T(12, '"', 13);
-	T(12, '\\', 14);
-	T(10, '\n', 11);
+    std::string all_chars;
+    for(unsigned char i = 0; i < 128; ++i)
+        all_chars.push_back(i);
+
+    L(1, alpha_symbol, 2);
+    L(3, alpha_symbol, 2);
+    L(4, alpha_symbol, 2);
+    L(5, alpha_symbol, 2);
+
+    L(2, alpha_number_symbol, 2);
+
+    L(1, number, 4);
+    L(3, number, 4);
+    L(4, number, 4);
+    L(5, number, 4);
+    
+    L(10, all_chars, 10);
+    L(12, all_chars, 12);
+    L(14, all_chars, 12);
+    
+	L(1, '"', 12);
+	L(1, '-', 3);
+	L(1, ';', 10);
+	L(1, ' ', 11);
+	L(1, '\t', 11);
+	L(1, '\n', 11);
+	L(1, '{', 6);
+	L(1, '}', 7);
+	L(1, '(', 8);
+	L(1, ')', 9);
+	L(4, '.', 5);
+	L(12, '"', 13);
+	L(12, '\\', 14);
+	L(10, '\n', 11);
 
     is << "#include \"lexical/Lexical.hh\"" << std::endl << std::endl;
     is << "Lexical::state_t Lexical::dfa_trans_table_[][128] =" << std::endl;
+
+    auto lmap = dfa.getMap();
 
 	is << "{" << std::endl;
 	for(size_t i = 0; i < STATE_NUM; ++i) {
